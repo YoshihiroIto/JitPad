bb8115d57781dcc0d8a5193f366b79fbf786c233
Yoshihiro Ito <yo.i.jewelry.bab@gmail.com>
2020-01-22 14:23:23 +0900

    Add JitPad.Foundation

---
+++ JitPad.Foundation/NotificationObject.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace JitPad.Foundation
+{
+    public class NotificationObject : INotifyPropertyChanged
+    {
+        protected bool SetProperty<T>(ref T storage, T value, [CallerMemberName] string propertyName = "")
+        {
+            if (EqualityComparer<T>.Default.Equals(storage, value))
+                return false;
+
+            storage = value;
+
+            RaisePropertyChanged(propertyName);
+
+            return true;
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected virtual void RaisePropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            // ReSharper disable once InconsistentlySynchronizedField
+            var pc = (PropertyChangedEventArgs)PropChanged[propertyName];
+
+            if (pc == null)
+            {
+                // double-checked;
+                lock (PropChanged)
+                {
+                    pc = (PropertyChangedEventArgs)PropChanged[propertyName];
+
+                    if (pc == null)
+                    {
+                        pc = new PropertyChangedEventArgs(propertyName);
+                        PropChanged[propertyName] = pc;
+                    }
+                }
+            }
+
+            PropertyChanged?.Invoke(this, pc);
+        }
+
+        // use Hashtable to get free lockless reading
+        private static readonly Hashtable PropChanged = new Hashtable();
+    }
+}
+
---
+++ JitPad.Foundation/ViewModelBase.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Reactive.Disposables;
+using System.Threading;
+
+namespace JitPad.Foundation
+{
+    public class ViewModelBase : NotificationObject, IDisposable
+    {
+        private CompositeDisposable? _Trashes;
+
+        public CompositeDisposable Trashes =>
+            LazyInitializer.EnsureInitialized(ref _Trashes, () => new CompositeDisposable()) ??
+            throw new NullReferenceException();
+
+        private bool _disposed;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (_disposed)
+                return;
+
+            if (disposing)
+                _Trashes?.Dispose();
+
+            _disposed = true;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
