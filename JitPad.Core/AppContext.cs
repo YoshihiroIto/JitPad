11ef96541fd6e4568074437d7217c4ee08fa39d8
Yoshihiro Ito <yo.i.jewelry.bab@gmail.com>
2020-01-23 13:13:08 +0900

    Remove Secondary ui

--- JitPad.Core/AppContext.cs
+++ JitPad.Core/AppContext.cs
@@ -6,12 +6,10 @@ namespace JitPad.Core
     public class AppContext : NotificationObject, IDisposable
     {
         public ProcessingUnit Primary { get; } = new ProcessingUnit();
-        public ProcessingUnit Secondary { get; } = new ProcessingUnit();
 
         public void Dispose()
         {
             Primary.Dispose();
-            Secondary.Dispose();
         }
     }
 }
