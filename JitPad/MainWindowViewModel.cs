2b2101353a31ee185b1a8fa0dcbb136d748e72fd
Yoshihiro Ito <yo.i.jewelry.bab@gmail.com>
2020-01-23 12:39:45 +0900

    Remove Secondary ui

--- JitPad/MainWindowViewModel.cs
+++ JitPad/MainWindowViewModel.cs
@@ -10,16 +10,10 @@ namespace JitPad
         public ReactiveProperty<string> PrimarySourceText { get; }
         public ReadOnlyReactiveProperty<string> PrimaryResult { get; }
 
-        public ReactiveProperty<string> SecondarySourceText { get; }
-        public ReadOnlyReactiveProperty<string> SecondaryResult { get; }
-
         public MainWindowViewModel(AppContext appContext)
         {
             PrimarySourceText = appContext.Primary.SourceFile.ToReactivePropertyAsSynchronized(x => x.Text).AddTo(Trashes);
             PrimaryResult = appContext.Primary.ObserveProperty(x => x.Result).ToReadOnlyReactiveProperty().AddTo(Trashes);
-            
-            SecondarySourceText = appContext.Secondary.SourceFile.ToReactivePropertyAsSynchronized(x => x.Text).AddTo(Trashes);
-            SecondaryResult = appContext.Secondary.ObserveProperty(x => x.Result).ToReadOnlyReactiveProperty().AddTo(Trashes);
         }
     }
 }
