c9102bf6cfa38ae2a725dbb46ee04cdc13bf2264
Yoshihiro Ito <yo.i.jewelry.bab@gmail.com>
2020-02-01 07:16:36 +0900

    Change about dialog to close by [ESC]

--- JitPad/AboutDialog.xaml.cs
+++ JitPad/AboutDialog.xaml.cs
@@ -1,10 +1,18 @@
-﻿namespace JitPad
+﻿using System.Windows.Input;
+
+namespace JitPad
 {
     public partial class AboutDialog
     {
         public AboutDialog()
         {
             InitializeComponent();
+        }
+
+        private void OnPreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Escape)
+                Close();
         }
     }
 }
